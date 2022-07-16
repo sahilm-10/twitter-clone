e5e0aabe518e7a1f7a367c2ddbac5567a286208f
sahilm-10 <malgundkarsahil10@gmail.com>
2022-07-16 16:10:41 +0530

    Widgets Section Added with twitter-embed lib

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import "./Widgets.css";
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
     <div className='widgets'>
-        <h2>Widgets</h2>
+        <div className='widgets__input'>
+          <SearchIcon className='widgets__searchIcon'/>
+          <input placeholder='Search Twitter' type = 'text'/>
+        </div>
+        <div className='widgets__widgetContainer'>
+          <h2>What's Happening</h2>
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />   
+        {/* your id */}
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+        {/* Your profile */}
+
+        <TwitterShareButton
+          url={"https://openboardd.herokuapp.com/"}
+          options={{ text: "Check out my Work! Desktop recommended", via: "sahilmalgundkar6" }}
+        />
+
+        </div>
     </div>
   );
 }
